e462e34d951239c488da811bc88295a0c084e4d0
RGordon93 <contactrashawngordon@gmail.com>
2020-12-13 17:53:41 -0500

    finished puo/urls.py

--- puo_django/urls.py
+++ puo_django/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('users.urls'))
+    path('', include('puo.urls')),
 ]
+
+
