9338a147cf0ff09b5469e0bb76e58413f41c43da
nickosp <nickospapageorge85@gmail.com>
2020-12-13 14:24:13 -0500

    serializers finished

--- puo/models.py
+++ puo/models.py
@@ -24,10 +24,10 @@ class Post(models.Model):
         return self.name
 
 class Endorse(models.Model):
-name = models.CharField(max_length=100)
-comment = models.CharField(max_length=200)
-created_at = models.DateTimeField(auto_now_add=True)
-updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
