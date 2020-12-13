0472980b5890e116bd9b7607c78bcf1eebffb150
nickosp <nickospapageorge85@gmail.com>
2020-12-13 13:35:52 -0500

    project submission model

--- puo/models.py
+++ puo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProjectSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    project_name = models.CharField(max_length=100)
+    project_description = models.CharField(max_length=500)
+    project_url = models.TextField(blank=True)
+    project_photo = models.ImageField(upload_to='media/images/', default='media/images/default.jpg')
+    portfolio_url = models.TextField(blank=True)
