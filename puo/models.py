b34560507a314d7df09e0d1c3cd7704a2a7471c9
nickosp <nickospapageorge85@gmail.com>
2020-12-13 13:50:26 -0500

    post model

--- puo/models.py
+++ puo/models.py
@@ -10,3 +10,12 @@ class ProjectSubmission(models.Model):
     project_url = models.TextField(blank=True)
     project_photo = models.ImageField(upload_to='media/images/', default='media/images/default.jpg')
     portfolio_url = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
